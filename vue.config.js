08095c5530abf3818afc7e706ecf8662c3674945
Floyd Pink <imbleedingme@googlemail.com>
2021-05-27 18:12:06 -0400

    update vue.config.js to implement Hot Module Replacement - closes #15

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,13 @@
 module.exports = {
+    devServer: {
+        host: 'localhost',
+        hot: true
+    },
     pages: {
         index: {
-            entry: "src/main.js",
-            template: "public/index.html",
-            title: "Kollavarsham Calendar [BETA]"
+            entry: 'src/main.js',
+            template: 'public/index.html',
+            title: 'Kollavarsham Calendar [BETA]'
         }
     },
     publicPath: process.env.NODE_ENV === 'production'
