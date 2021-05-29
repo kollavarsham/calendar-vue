54a0b46171c0228e50e2e2c9883304060b9dd7b9
Dhruva Srinivas <66675022+carrotfarmer@users.noreply.github.com>
2021-05-29 22:17:27 +0530

    [UPDATED] Add Footer & banner (#14)
    * add footer
    
    * add footer
    
    * add footer
    
    * remove unused deps & fix some CSS stuff
    
    * add under construction banner
    
    * fix package.json and update styles
    
    * fix package.json and update styles
    
    * add @vue/cli-service
    
    * add vue-template-compiler
    
    * make @vue/cli-service a dev dep

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Calendar />
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Footer from "./components/Footer";
+import Calendar from "./components/Calendar";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Footer,
+    Calendar
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+  font-family: "Roboto", sans-serif;
+  -webkit-font-smoothing: antialiased; 
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer>
+    <div class="container">
+      <p class="float-left">
+        <small>
+          <a href="#">Version</a>
+        </small>
+      </p>
+      <p class="float-right">
+        <small>
+          <a href="https://kollavarsham.org/LICENSE.txt">&copy; 2015-2021 The Kollavarsham Team</a>
+        </small>
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+footer {
+  padding-top: 5px;
+  border-top: 1px solid #e5e5e5;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 30px;
+  background-color: #f5f5f5;
+  z-index: 9;
+}
+</style>
