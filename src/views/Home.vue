e0fb3051db016ba52146ca295b7d0b91e859d962
Floyd Pink <imbleedingme@googlemail.com>
2021-06-03 21:00:17 -0400

    recreate the Vue.js boilerplate using vue-cli  - convert to using TypeScript and class-style components  - add Vuex and vue-router  - add unit and e2e tests placeholders  - add on-save and on-commit linters

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <Header />
+    <Calendar />
+    <Footer />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+import Header from '@/components/Header.vue';
+import Calendar from '@/components/Calendar.vue';
+import Footer from '@/components/Footer.vue';
+
+@Component({
+  components: {
+    Header,
+    Calendar,
+    Footer,
+  },
+})
+export default class Home extends Vue {}
+</script>
